6eed9d48bf7ef2ff4ea1aa26343d714e24fcf68e
david <drevelo@unicomfacauca.edu.co>
2025-03-29 11:45:33 -0500

    add cors configuration

--- app3.py
+++ app3.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from routers import routerPredictions
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(routerPredictions.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes cambiar "*" por ["http://127.0.0.1:5500"] si quieres más seguridad
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos los encabezados
+)
 
 if __name__=="__main__":
     app.run()
